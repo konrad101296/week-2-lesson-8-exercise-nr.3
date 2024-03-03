df1231640f5e4227a0fc05d24fcf76398600dece
konrad101296 <konradslimak@o2.pl>
2024-03-03 09:53:03 +0100

    Add project files.

---
+++ week 2 lesson 8 exercise nr.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+int wynik;
+int a = 1;
+int b = 1;
+int c;
+int d;
+Console.WriteLine(a);
+Console.WriteLine(b);
+for (int i = 1; i < 45; i++)
+{
+   
+    c = a + b;
+    d = b;
+    b = c;
+    a = d;
+    Console.WriteLine(c);
+}
